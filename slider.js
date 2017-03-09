bc6f9ee1027536e48133d435ef657f688bb3d609
jeremN <jeremne@gmail.com>
2017-03-09 15:47:42 +0100

    Added more -don't work

--- slider.js
+++ slider.js
@@ -8,7 +8,12 @@
 		$slideNavNext = $('#hSlideNext'),
 		$slider = $('.h-slider'),
 		$currentSlide = 0,
+		$currentThumb = 1,
+		$slideTo = 'left',
+		$size = $('.left-image .item').length,
 		$easing = Power3.easeInOut;
+
+		console.log($size);
 
 	//Init
 	function init(){
@@ -19,18 +24,69 @@
 
 		//Set the previous button inactive
 		TweenLite.set($slideNavPrev, {autoAlpha: 0.2});
+
+		//Get url of <img> and place it as background img
+		$('.preview, .item').each(function(){
+
+			var imgSrc = $(this).children('img').attr('src');
+			$(this).css('background-image', 'url('+ imgSrc +')');
+		});
 	}
 
 	init();
 
+
 	/*Slide Functions*/
-	function goToNext(){
+	function goToNext(object, direction){
 
+		var slideObj = object,
+			slideDirection,
+			slideTl = new TimelineLite(),
+			slideOut = $slide.hasClass('isActive'),
+			slideIn = $('.slide.isActive').next('.slide'),
+			toLeft = '+=100%',
+			toRight = '-=100%';
+
+
+		//Set slide direction
+		if(direction === 'left' || direction === 1){
+
+			slideDirection = 1;
+		} 
+		else{
+			slideDirection = -1;
+		}
+
+		//Animate
+		slideTl
+			.set(slideIn, {autoAlpha: 1, className: '+=active'})
+			.set(slideOut, {className: "-=active"})
+			.to(slideObj.eq(currentSlide), 1, {x: toLeft});
+
+		if(slideDirection === 1){
+
+			if(currentSlide < slideObj.length -1){
+
+				currentSlide++;
+			}
+			else{
+			
+				currentSlide = 0;
+			}
+		}
+		else{
+
+			if(currentSlide > 0){
+
+				currentSlide--;
+			}
+			else{
+
+				currentSlide = slideObj.length - 1; 
+			}
+		}
 	}
 
-	function goToPrev(){
-
-	}
 
 	/*Navigation*/
 	//Go to previous
@@ -38,7 +94,8 @@
 
 		e.preventDefault();
 
-		goToPrev();
+		slideTo = 'left';
+		goToNext($slide, slideTo);
 
 	});
 
