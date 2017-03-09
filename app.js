27784092edfb85ffaeafdedfcc09c049bf7ac636
jeremN <jeremne@gmail.com>
2017-03-09 09:18:27 +0100

    base thumbs function added

--- app.js
+++ app.js
@@ -30,16 +30,19 @@
     function goToNext(slideOut, slideIn, previewIn, previewOut){
 
         var slideTl = new TimelineLite(),
+            thumbTl = new TimelineLite(),
             slideOutLeft = slideOut.find('> .split-left img'),
             slideOutRight = slideOut.find('> .split-right img'),
             slideInLeft = slideIn.find('> .split-left img'),
             slideInRight = slideIn.find('> .split-right img'),
+            lastPreview = previewOut.prev('.preview'),
             index = slideIn.index(),
             size = $('#slider .slide').length,
-            thumbTl = new TimelineLite(),
-            lastPreview = previewOut.prev('.preview'),
             thumbIndex = previewIn.index(),
             thumbSize = $('#slide-preview .preview').length;
+
+        console.log("N slide index is " + index);
+        console.log("N slide size is " + size);
 
         //Prevent going to blank side
         if( slideIn.length !== 0 ){
@@ -54,28 +57,24 @@
               .to(slideInRight, 0.5, {x:'+=50%', ease: $easing}, 0.35)
               .to(slideOutRight, 1, {x:'+=100%', ease: $easing}, 0.5);
 
-        }
 
-        //Go to next thumb 
-        if( previewIn.length !== 0 ){
 
-            thumbTl
-              .set(previewIn, {x:'-100%', autoAlpha: 1, className: '+=active'})
-              .set(previewOut, {className: '-=active'})
-              .to(previewOut, 0.15, {x: '+=100%', ease: $easing}, 0)
-              .to(previewIn, 0.15, {x: '+=100%', ease: $easing}, 0);
-            
-        }
+            if( index === size ){
 
-        if( index <= 0 ){
-
-            thumbTl.reverse();
-            /*
-              .set(previewIn, {x: '0', className: '-=active'})
-              .set(lastPreview, {x: '100%', autoAlpha: 1, className: '+=active'})
-              .to(lastPreview, 0.15, {x: '-=100%', ease: $easing})
-              .to(previewIn, 0.15, {x: '-=100%', ease: $easing});
-            */
+                thumbTl
+                  .set(previewIn, {x: '0', className: '-=active'})
+                  .set(lastPreview, {x: '100%', autoAlpha: 1, className: '+=active'})
+                  .to(lastPreview, 0.15, {x: '-=100%', ease: $easing})
+                  .to(previewIn, 0.15, {x: '-=100%', ease: $easing});
+            }
+            else{
+                
+                thumbTl
+                  .set(previewIn, {x:'-100%', autoAlpha: 1, className: '+=active'})
+                  .set(previewOut, {className: '-=active'})
+                  .to(previewOut, 0.15, {x: '+=100%', ease: $easing}, 0)
+                  .to(previewIn, 0.15, {x: '+=100%', ease: $easing}, 0);
+            }
         }
 
         //Fade in arrow down
@@ -91,19 +90,24 @@
     function goToPrevious(slideOut, slideIn, previewIn, previewOut){
 
         var slideTl = new TimelineLite(),
+            thumbTl= new TimelineLite(),
             slideOutLeft = slideOut.find('>.split-left img'),
             slideOutRight = slideOut.find('>.split-right img'),
             slideInLeft = slideIn.find('>.split-left img'),
             slideInRight = slideIn.find('>.split-right img'),
+            lastPreview = previewOut.next('.preview'),
             index = slideIn.index(),
             size = $('#slider .slide').length,
-            thumbTl= new TimelineLite(),
             thumbIndex = previewIn.index(),
             thumbSize = $('#slide-preview .preview').length;
+
+        console.log("P slide index is " + index);
+        console.log("P slide size is " + size);
 
         /*Go to next slide*/
         //Prevent going to blank side
         if( slideIn.length !== 0 ){
+
 
             slideTl
               .set(slideIn, {autoAlpha: 1, className: '+=active'})
@@ -115,24 +119,24 @@
               .to(slideInLeft, 0.45, {x:'-=50%', ease: $easing}, 0.15)
               .to(slideOutLeft, 0.5, {x:'-=100%', ease: $easing}, 0.5);
 
+
+            if( index === 1){
+
+                thumbTl
+                  .set(previewIn, {x: '0', className: '-=active'})
+                  .set(lastPreview, {x: '0', autoAlpha: 1, className: '+=active'})
+                  .to(lastPreview, 0.15, {x: '-=100%', ease: $easing})
+                  .to(previewIn, 0.15, {x: '-=100%', ease: $easing});
+            }
+            else{
+                
+                thumbTl
+                  .set(previewIn, {x:'-100%', autoAlpha: 1, className: '+=active'})
+                  .set(previewOut, {className: '-=active'})
+                  .to(previewOut, 0.15, {x: '+=100%', ease: $easing}, 0)
+                  .to(previewIn, 0.15, {x: '+=100%', ease: $easing}, 0);
+            }
         }
-
-        /*Go to previous thumb*/
-        //Prevent going to blank side
-        if( previewIn.length !== 0 ){
-
-            thumbTl
-              .set(previewIn, {x:'-100%', autoAlpha: 1, className: '+=active'})
-              .set(previewOut, {className: '-=active'})
-              .to(previewOut, 0.15, {x: '+=100%', ease: $easing}, 0)
-              .to(previewIn, 0.15, {x: '+=100%', ease: $easing}, 0);
-        }
-
-        if( index <= 0 ){
-
-            thumbTl.reverse();
-        }
-
 
         //Fade in arrow down
         TweenLite.set($slideNavNext, {autoAlpha: 1});
@@ -143,66 +147,6 @@
         }
     }
 
-    /**Next & Previous preview img function**/
-    //Go to Next preview
-    /*
-    function goToNextPreview(previewIn, previewOut){
-
-        var thumbTl = new TimelineLite(),
-            lastPreview = previewOut.prev('.preview'),
-            thumbIndex = previewIn.index(),
-            thumbSize = $('#slide-preview .preview').length;
-
-        console.log("N prevIn length is " + previewIn.length)
-        console.log("N size is " + size);
-        console.log("N index is " + index);
-
-        if( previewIn.length !== 0 ){
-
-            thumbTl
-              .set(previewIn, {x:'-100%', autoAlpha: 1, className: '+=active'})
-              .set(previewOut, {className: '-=active'})
-              .to(previewOut, 0.15, {x: '+=100%', ease: $easing}, 0)
-              .to(previewIn, 0.15, {x: '+=100%', ease: $easing}, 0);
-            
-            if( index <= 0 ){
-
-                tl
-                  .set(previewIn, {x: '0', className: '-=active'})
-                  .set(lastPreview, {x: '100%', autoAlpha: 1, className: '+=active'})
-                  .to(lastPreview, 0.15, {x: '-=100%', ease: $easing})
-                  .to(previewIn, 0.15, {x: '-=100%', ease: $easing});
-                
-            }
-        }
-    }*/
-
-    //Go to Previous preview
-    /*
-    function goToPrevPreview(previewIn, previewOut){
-
-        var tl= new TimelineLite(),
-            index = previewIn.index(),
-            size = $('#slide-preview .preview').length;
-
-            console.log("P size is " + size);
-            console.log("P index is " + index);
-
-
-        if( previewIn.length !== 0 ){
-
-            tl
-              .set(previewIn, {x:'-100%', autoAlpha: 1, className: '+=active'})
-              .set(previewOut, {className: '-=active'})
-              .to(previewOut, 0.15, {x: '+=100%', ease: $easing}, 0)
-              .to(previewIn, 0.15, {x: '+=100%', ease: $easing}, 0);
-        }
-
-        if( index <= 0){
-
-            tl.reverse();
-        }
-    }*/
 
     /**Nav click**/
     //Go to next slide
@@ -215,7 +159,6 @@
             previewOut = $('.preview.active'),
             previewIn = $('.preview.active').next('.preview'); 
 
-        //goToNextPreview(previewIn, previewOut);
         goToNext(slideOut, slideIn, previewIn, previewOut);   
     });
 
@@ -229,7 +172,6 @@
             previewOut = $('.preview.active'),
             previewIn = $('.preview.active').prev('.preview'); 
 
-        //goToPrevPreview(previewIn, previewOut);    
         goToPrevious(slideOut, slideIn, previewIn, previewOut);
     });
 
